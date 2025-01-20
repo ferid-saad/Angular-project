378356739e535bbbb84d81e34f83efe9e34917a6
SAAD FERID <fersd2018@gmail.com>
2025-01-21 00:51:28 +0100

    from ts to html

--- helloword/src/app/app.component.ts
+++ helloword/src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'helloword';
-}
+title ="angular";
+condition=true;
+number=123;
+source="assets/img_angular.png"
+user1="saleh";
+user2="med";
+user3="sarrah";
+namelist =['Ahmed','ferid','aziza'];
+  }
